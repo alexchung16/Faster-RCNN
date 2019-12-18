99332ea0156843d0244fc048d96a45435bef9024
alexchung <yonganzhong@outlook.com>
2019-12-18 15:16:59 +0800

    delete resnet

--- faster_rcnn_util/cfgs.py
+++ faster_rcnn_util/cfgs.py
@@ -118,3 +118,30 @@ FAST_RCNN_MINIBATCH_SIZE = 256  # if is -1, that is train with OHEM
 FAST_RCNN_POSITIVE_RATE = 0.25
 
 ADD_GTBOXES_TO_TRAIN = False
+
+
+#---------------------------------------label_dict----------------------------
+PASCAL_NAME_LABEL_MAP = {
+    'back_ground': 0,
+    'aeroplane': 1,
+    'bicycle': 2,
+    'bird': 3,
+    'boat': 4,
+    'bottle': 5,
+    'bus': 6,
+    'car': 7,
+    'cat': 8,
+    'chair': 9,
+    'cow': 10,
+    'diningtable': 11,
+    'dog': 12,
+    'horse': 13,
+    'motorbike': 14,
+    'person': 15,
+    'pottedplant': 16,
+    'sheep': 17,
+    'sofa': 18,
+    'train': 19,
+    'tvmonitor': 20
+}
+
---
+++ faster_rcnn_util/losses.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : losses.py
+# @ Description:  
+# @ Author     : jemmy li
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2019/12/18 PM 15:05
+# @ Software   : PyCharm
+#-------------------------------------------------------
+
+import tensorflow as tf
+
